e39478e562f60a3adc25d2e94c66c66372aff67e
Veerendra-reddy-medapati <veerendrav863@gmail.com>
2026-06-05 21:15:24 +0530

    fix vercel root directory

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,15 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://prompts-website-five.vercel.app/",
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 /* =========================
